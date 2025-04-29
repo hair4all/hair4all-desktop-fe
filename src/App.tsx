d2acb16b844cc18d064bdeb85304f5833e9af301
goraindiraja <gora.indiraja@gmail.com>
2025-04-30 00:19:51 +0800

    (init) Initial Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen">
+        <h1 className="text-lg underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
